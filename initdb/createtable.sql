96de17d4588eb7978f9388249a4d9edf0aafcd0c
0xhajar <hajar.haouat@student.vinci.be>
2024-12-03 13:38:03 +0100

    feat: insert users data

--- initdb/createtable.sql
+++ initdb/createtable.sql
@@ -4,3 +4,9 @@ CREATE TABLE IF NOT EXISTS pfe.hello (
     id SERIAL PRIMARY KEY,
     text TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS pfe.users (
+    id SERIAL PRIMARY KEY,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+);
--- initdb/insert.sql
+++ initdb/insert.sql
@@ -1,1 +1,5 @@
 INSERT INTO pfe.hello (id, text) VALUES (1, 'j''ai changé je le jure');
+INSERT INTO pfe.users (email, password)
+VALUES 
+('admin@example.com', 'password'), 
+('user1@example.com', 'password');
