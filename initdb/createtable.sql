3ca9fac16a63da187b9f4cbfff3f2f266b239383
hadje <hadjera.emroska@student.vinci.be>
2024-12-03 16:48:00 +0100

    feat: add questions table to database schema

--- initdb/createtable.sql
+++ initdb/createtable.sql
@@ -10,3 +10,11 @@ CREATE TABLE IF NOT EXISTS pfe.users (
     email TEXT NOT NULL,
     password TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS pfe.questions (
+    id SERIAL PRIMARY KEY,
+    question_status VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    sub_category VARCHAR(255) NOT NULL,
+    question TEXT NOT NULL
+);
