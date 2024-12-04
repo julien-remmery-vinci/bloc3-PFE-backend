f7dff2ea7ed2c40d27a7a3232d1a97b59fb68461
hadje <hadjera.emroska@student.vinci.be>
2024-12-04 20:34:59 +0100

    refactor: rename answerError enum to AnswerError and update implementation

--- src/errors/answer_error.rs
+++ src/errors/answer_error.rs
@@ -1,21 +1,21 @@
 use axum::{http::StatusCode, response::IntoResponse};
 
-pub enum answerError {
+pub enum AnswerError {
     Conflict,
     BadRequest,
     DbError(sqlx::Error),
 }
 
-impl IntoResponse for answerError {
+impl IntoResponse for AnswerError {
     fn into_response(self) -> axum::response::Response {
         let response = axum::http::Response::builder();
         let (code, message) = match self {
-            answerError::DbError(e) => {
+            AnswerError::DbError(e) => {
                 println!("db error : {:?}", e); //tracing::warn!() plutot que println!() mais bon...
                 (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error")
             }
-            answerError::Conflict => (StatusCode::CONFLICT, "Answer already exists"),
-            answerError::BadRequest => (StatusCode::BAD_REQUEST, "Bad request"),
+            AnswerError::Conflict => (StatusCode::CONFLICT, "Answer already exists"),
+            AnswerError::BadRequest => (StatusCode::BAD_REQUEST, "Bad request"),
         };
         let body = axum::body::Body::from(message);
         response.status(code).body(body).unwrap()
