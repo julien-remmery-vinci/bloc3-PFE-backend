012224fb3e30cbd031fcb8cbac695f2b7d44d1ad
0xhajar <hajar.haouat@student.vinci.be>
2024-12-05 17:49:06 +0100

    fix: migrations script

--- migrations/1_createtables.sql
+++ migrations/1_createtables.sql
@@ -1,5 +1,17 @@
-DROP SCHEMA IF EXISTS pfe CASCADE;
-CREATE SCHEMA pfe;
+CREATE SCHEMA IF NOT EXISTS pfe;
+
+CREATE TABLE IF NOT EXISTS pfe.users (
+    id SERIAL PRIMARY KEY,
+    login TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS pfe.questions (
+    id SERIAL PRIMARY KEY,
+    category VARCHAR(255) NOT NULL,
+    sub_category VARCHAR(255) NOT NULL,
+    question TEXT NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS pfe.companies (
     company_id SERIAL PRIMARY KEY,
@@ -16,58 +28,17 @@ CREATE TABLE IF NOT EXISTS pfe.companies (
     dispute BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE IF NOT EXISTS pfe.users (
-    id SERIAL PRIMARY KEY,
-    firstname TEXT NOT NULL,
-    lastname TEXT NOT NULL,
-    login TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    role TEXT NOT NULL CHECK (role IN ('user', 'admin')),
-    company_id INTEGER REFERENCES pfe.companies(company_id) NULL
-);
-
-CREATE TABLE IF NOT EXISTS pfe.questions (
-    id SERIAL PRIMARY KEY,
-    question_status VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL,
-    sub_category VARCHAR(255) NOT NULL,
-    question TEXT NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS pfe.forms (
     form_id SERIAL PRIMARY KEY,
     company INTEGER REFERENCES pfe.companies(company_id),
     type VARCHAR(10) CHECK (type IN ('ODD', 'ESG')),
     nb_questions INTEGER,
-    template TEXT
+    template VARCHAR(15)
 );
 
 CREATE TABLE IF NOT EXISTS pfe.questions_form (
     form_id INTEGER REFERENCES pfe.forms(form_id),
     question_id INTEGER REFERENCES pfe.questions(id),
+    question_status VARCHAR(255) NOT NULL,
     PRIMARY KEY (form_id, question_id)
 );
-
-CREATE TABLE IF NOT EXISTS pfe.answers (
-    answer_id SERIAL PRIMARY KEY,
-    answer TEXT NOT NULL,
-    template TEXT NOT NULL,
-    question_id INTEGER REFERENCES pfe.questions(id),
-    score DOUBLE PRECISION NOT NULL,
-    engagement_score DOUBLE PRECISION,
-    is_forced_engagement BOOLEAN NOT NULL,
-    comment TEXT
-);
-
-CREATE TABLE IF NOT EXISTS pfe.user_answer_esg (
-    answer_id INTEGER REFERENCES pfe.answers(answer_id),
-    user_id INTEGER REFERENCES pfe.users(id),
-    form_id INTEGER REFERENCES pfe.forms(form_id),
-    now BOOLEAN NOT NULL,
-    commitment_pact BOOLEAN NOT NULL,
-    comment TEXT,
-    now_verif BOOLEAN,
-    commitment_pact_verif BOOLEAN,
-    PRIMARY KEY (answer_id, user_id, form_id)
-)
-
