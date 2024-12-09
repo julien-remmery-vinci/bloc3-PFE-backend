5448526d7cb392abb694683fc7f415929431caac
hadje <hadjera.emroska@student.vinci.be>
2024-12-09 02:50:58 +0100

    feat: update ScoreQuery struct to include sub_category and optional score

--- src/models/score.rs
+++ src/models/score.rs
@@ -3,11 +3,15 @@ use sqlx::prelude::FromRow;
 
 #[derive(Deserialize, FromRow, Serialize, Debug)]
 pub struct ScoreQuery {
-    pub form_id: i32,
+    pub sub_category: String,
+    pub score: Option<f64>
 }
 
 impl ScoreQuery {
-    pub fn new(form_id: i32) -> Self {
-        Self { form_id }
+    pub fn new(sub_category: String, score: Option<f64>) -> Self {
+        Self {
+            sub_category,
+            score
+        }
     }
 }
